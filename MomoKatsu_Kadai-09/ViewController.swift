73ec2f46986f007fec2c536decdac168e9369fba
momokatsu3 <moka.oct10@gmail.com>
2023-06-09 22:22:16 +0900

    First Version

---
+++ MomoKatsu_Kadai-09/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MomoKatsu_Kadai-09
+//  Created by モモカツ on 2023/06/07.
+// Part9 Modal(モーダル)画面で都道府県を選択するアプリ
+
+// 【Swift/Xcode超入門】画面遷移するアプリを作ってみよう
+// https://satoriku.com/app-dev-step4/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // 選択したテーブルビューを試験的に表示するラベル
+    @IBOutlet weak var displaySelsectValue: UILabel! 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // コンテンツビューが画面に表示される前に実行する必要のある操作を実行
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //AppDelegateから変数の値を受け取る
+        //AppDelegateのインスタンスを取得
+        let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        let selsectTablaValue = appDelegate.selsectTablaValueDelgate
+
+        print ("モーダル画面のテーブルビューで選択した値：", selsectTablaValue)
+        displaySelsectValue.text = "\(selsectTablaValue)"
+    }
+
+}
+
